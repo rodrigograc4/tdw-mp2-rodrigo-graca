d2cbed4c91232253092b3df6c67ba98fc5284691
Rodrigo Graça <rodrigomgraca@ua.pt>
2024-11-25 21:37:49 +0000

    esqueleto melhorado

--- pokeapollo/src/components/PokedexGrid.jsx
+++ pokeapollo/src/components/PokedexGrid.jsx
@@ -1,15 +1,24 @@
-// PokemonGrid.js
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const PokedexGrid = ({ pokemons, isLoading, searchTerm }) => {
     const navigate = useNavigate();
 
+    const [loadedImages, setLoadedImages] = useState({});
+
+    const handleImageLoad = (id) => {
+        setLoadedImages((prev) => ({ ...prev, [id]: true }));
+    };
+
     const SkeletonCard = () => (
-        <div className="bg-white rounded-lg shadow-md border border-gray-300 flex items-center p-2">
-            <div className="w-[5.25em] aspect-square mx-4 rounded bg-gray-300 animate-pulse"></div>
-            <div className="h-8 bg-gray-300 rounded w-3/4 animate-pulse"></div>
-            <div className="h-8 w-12 bg-gray-300 rounded mx-4 animate-pulse"></div>
+        <div className="p-1 bg-white rounded-lg shadow-md border border-gray-300 flex items-center cursor-pointer">
+            <div className="w-[5.25em] h-[5.25em] mx-4 flex items-center justify-center relative">
+                <div className="absolute w-full h-full rounded bg-gray-300 animate-pulse"></div>
+            </div>
+            <div className="flex-grow">
+                <div className="h-8 bg-gray-300 rounded w-3/4 animate-pulse"></div>
+            </div>
+            <div className="w-12 h-8 bg-gray-300 rounded mx-4 animate-pulse"></div>
         </div>
     );
 
@@ -21,20 +30,29 @@ const PokedexGrid = ({ pokemons, isLoading, searchTerm }) => {
         <div className="flex-grow overflow-auto p-4 scrollbar-hide">
             <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
                 {isLoading
-                    ? Array.from({ length: 18 }).map((_, index) => (
+                    ? Array.from({ length: 21 }).map((_, index) => (
                         <SkeletonCard key={index} />
                     ))
                     : filteredPokemons.map((pokemon) => (
                         <div
                             key={pokemon.id}
                             onClick={() => navigate(`/pokedex/${pokemon.id}`)}
-                            className="bg-white rounded-lg shadow-md border border-gray-300 flex items-center cursor-pointer hover:shadow-lg transition"
+                            className="p-1 bg-white rounded-lg shadow-md border border-gray-300 flex items-center cursor-pointer hover:shadow-lg transition"
                         >
-                            <img
-                                src={pokemon.image}
-                                alt={pokemon.name}
-                                className="h-20 w-20 mx-4 object-contain"
-                            />
+                            <div className="w-[5.25em] h-[5.25em] mx-4 relative">
+                                {/* Esqueleto */}
+                                {!loadedImages[pokemon.id] && (
+                                    <div className="absolute w-full h-full rounded bg-gray-300 animate-pulse"></div>
+                                )}
+                                {/* Imagem */}
+                                <img
+                                    src={pokemon.image}
+                                    alt={pokemon.name}
+                                    className={`absolute w-full h-full object-contain transition-opacity duration-300 ${loadedImages[pokemon.id] ? 'opacity-100' : 'opacity-0'
+                                        }`}
+                                    onLoad={() => handleImageLoad(pokemon.id)}
+                                />
+                            </div>
                             <div className="flex-grow">
                                 <p className="text-3xl text-black">{pokemon.name}</p>
                             </div>
