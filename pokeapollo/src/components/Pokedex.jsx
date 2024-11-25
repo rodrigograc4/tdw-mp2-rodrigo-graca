97f8ab7304a9330bd08b41527e63fab32314cd98
Rodrigo Graça <rodrigomgraca@ua.pt>
2024-11-25 15:51:51 +0000

    fix grid e size

--- pokeapollo/src/components/Pokedex.jsx
+++ pokeapollo/src/components/Pokedex.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 
 const Pokedex = () => {
     const [pokemons, setPokemons] = useState([]);
-    const [isLoading, setIsLoading] = useState(true); // Estado de carregamento
+    const [isLoading, setIsLoading] = useState(true);
 
     // Função para buscar dados dos Pokémon
     const fetchPokemonData = async () => {
@@ -34,44 +34,48 @@ const Pokedex = () => {
     const SkeletonCard = () => (
         <div className="bg-white rounded-lg shadow-md border border-gray-300 flex items-center p-2">
             <div className="w-[5.25em] aspect-square mx-4 rounded bg-gray-300 animate-pulse"></div>
-            <div className="h-8 bg-gray-300 rounded w-3/4 animate-pulse "></div>
-            <div className="h-8 w-12 bg-gray-300 rounded mx-4 animate-pulse "></div>
+            <div className="h-8 bg-gray-300 rounded w-3/4 animate-pulse"></div>
+            <div className="h-8 w-12 bg-gray-300 rounded mx-4 animate-pulse"></div>
         </div>
     );
 
     return (
-        <div className="relative w-2/3 h-[66vh] bg-red-600 rounded-lg border-2 border-black p-4 overflow-auto scrollbar-hide">
+        <div className="relative w-2/3 h-[66vh] bg-red-600 rounded-lg border-2 border-black flex flex-col">
             {/* Topo */}
-            <img className="absolute top-2 left-10 h-12" src="luzes.svg" alt="Botão azul" />
-            <div className="absolute top-16 left-0 w-full h-0.5 bg-black"></div>
+            <div className="flex-none p-4">
+                <img className="absolute top-2 left-10 h-12" src="luzes.svg" alt="Botão azul" />
+                <div className="absolute top-16 left-0 w-full h-0.5 bg-black"></div>
+            </div>
 
-            {/* Grid de Cards */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-16">
-                {isLoading
-                    ? Array.from({ length: 18 }).map((_, index) => (
-                        <SkeletonCard key={index} />
-                    ))
-                    : pokemons.map((pokemon) => (
-                        <div
-                            key={pokemon.id}
-                            className="bg-white rounded-lg shadow-md border border-gray-300 flex items-center"
-                        >
-                            {/* Imagem do Pokémon */}
-                            <img
-                                src={pokemon.image}
-                                alt={pokemon.name}
-                                className="h-20 w-20 mx-4 object-contain"
-                            />
+            {/* Grid Scroll */}
+            <div className="flex-grow mt-8 overflow-auto p-4 scrollbar-hide">
+                <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
+                    {isLoading
+                        ? Array.from({ length: 18 }).map((_, index) => (
+                            <SkeletonCard key={index} />
+                        ))
+                        : pokemons.map((pokemon) => (
+                            <div
+                                key={pokemon.id}
+                                className="bg-white rounded-lg shadow-md border border-gray-300 flex items-center"
+                            >
+                                {/* Imagem do Pokémon */}
+                                <img
+                                    src={pokemon.image}
+                                    alt={pokemon.name}
+                                    className="h-20 w-20 mx-4 object-contain"
+                                />
 
-                            {/* Nome do Pokémon */}
-                            <div className="flex-grow">
-                                <p className="text-2xl font-bold text-black">{pokemon.name}</p>
+                                {/* Nome do Pokémon */}
+                                <div className="flex-grow">
+                                    <p className="text-3xl text-black">{pokemon.name}</p>
+                                </div>
+
+                                {/* Número do Pokémon */}
+                                <p className="mx-4 text-gray-400 text-3xl">{`#${pokemon.id}`}</p>
                             </div>
-
-                            {/* Número do Pokémon */}
-                            <p className="mx-4 text-gray-400 text-2xl font-semibold">{`#${pokemon.id}`}</p>
-                        </div>
-                    ))}
+                        ))}
+                </div>
             </div>
         </div>
     );
