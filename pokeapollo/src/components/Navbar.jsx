434b3f2e62d2631857f21fee9a6846d60c982d0c
Rodrigo Graça <rodrigomgraca@ua.pt>
2024-11-25 20:39:27 +0000

    navbar responsiva

--- pokeapollo/src/components/Navbar.jsx
+++ pokeapollo/src/components/Navbar.jsx
@@ -4,14 +4,14 @@ import Menu from './Menu';
 const Navbar = () => {
 
     return (
-        <nav className="w-full fixed top-0 flex items-center justify-between px-60 py-7 mb-20 z-10">
+        <nav className="w-3/4 fixed top-0 left-1/2 transform -translate-x-1/2 flex items-center lg:justify-between justify-center py-7 mb-20 z-10">
             {/* Logo à esquerda */}
-            <div className="flex items-center space-x-3 px-6 py-2.5 rounded-full text-white text-3xl bg-menu-green">
+            <div className="hidden lg:flex items-center space-x-3 px-6 py-2.5 rounded-full text-white text-3xl bg-menu-green">
                 <img src="../logo.png" alt="User profile" className="h-7 w-7 rounded-full" />
-                <span>Poke Apollo</span>
+                <span className="hidden sm:block">Poke Apollo</span> {/* Exibe somente em telas maiores */}
             </div>
 
-            {/* Menu à direita */}
+            {/* Menu à direita (sempre visível) */}
             <Menu />
         </nav>
     );
