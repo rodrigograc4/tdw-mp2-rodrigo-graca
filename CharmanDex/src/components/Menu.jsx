c9c102eefbd056a7a6000c66eadb5748bfa5f713
Rodrigo Graça <rodrigomgraca@ua.pt>
2024-11-30 23:21:21 +0000

    eslint fix 3

--- CharmanDex/src/components/Menu.jsx
+++ CharmanDex/src/components/Menu.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, useMemo } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 const Menu = () => {
